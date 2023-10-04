ec8cd02cb151b42d4cbd9e419a44352d5f8b08fe
jeherold <jeherold@teksystems.com>
2023-10-04 15:14:49 -0400

    initial starter files to component with some initial style settings

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,9 +1,11 @@
+import PageNav from '../components/PageNav';
 // Uses the same styles as Product
-import styles from "./Product.module.css";
+import styles from './Product.module.css';
 
 export default function Product() {
   return (
     <main className={styles.product}>
+      <PageNav />
       <section>
         <div>
           <h2>
